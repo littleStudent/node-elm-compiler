816a859195563b9b763345b59c36608f07232748
Thomas Sattlecker <thomas.sattlecker@gmail.com>
2017-11-07 22:03:44 +0100

    a

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 'use strict';
 
-var spawn = require("cross-spawn");
-var _ = require("lodash");
-var compilerBinaryName = "elm-make";
-var fs = require("fs");
-var path = require("path");
-var temp = require("temp").track();
-var findAllDependencies = require("find-elm-dependencies").findAllDependencies;
+var spawn = require('cross-spawn');
+var _ = require('lodash');
+var compilerBinaryName = 'elm-make';
+var fs = require('fs');
+var path = require('path');
+var temp = require('temp').track();
+var findAllDependencies = require('find-elm-dependencies').findAllDependencies;
 
-var defaultOptions     = {
+var defaultOptions = {
   emitWarning: console.warn,
-  spawn:      spawn,
-  cwd:        undefined,
+  spawn: spawn,
+  cwd: undefined,
   pathToMake: undefined,
-  yes:        undefined,
-  help:       undefined,
-  output:     undefined,
-  report:     undefined,
-  warn:       undefined,
-  debug:      undefined,
-  verbose:    true,
+  yes: undefined,
+  help: undefined,
+  output: undefined,
+  report: undefined,
+  warn: undefined,
+  debug: undefined,
+  verbose: true,
   processOpts: undefined,
 };
 
 var supportedOptions = _.keys(defaultOptions);
 
 function prepareSources(sources) {
-  if (!(sources instanceof Array || typeof sources === "string")) {
-    throw "compile() received neither an Array nor a String for its sources argument.";
+  if (!(sources instanceof Array || typeof sources === 'string')) {
+    throw 'compile() received neither an Array nor a String for its sources argument.';
   }
 
-  return typeof sources === "string" ? [sources] : sources;
+  return typeof sources === 'string' ? [sources] : sources;
 }
 
 function prepareOptions(options, spawnFn) {
@@ -45,30 +45,39 @@ function prepareProcessArgs(sources, options) {
 }
 
 function prepareProcessOpts(options) {
-  var env = _.merge({LANG: 'en_US.UTF-8'}, process.env);
-  return _.merge({ env: env, stdio: "inherit", cwd: options.cwd }, options.processOpts);
-
+  var env = _.merge({ LANG: 'en_US.UTF-8' }, process.env);
+  return _.merge(
+    { env: env, stdio: 'inherit', cwd: options.cwd },
+    options.processOpts,
+  );
 }
 
 function runCompiler(sources, options, pathToMake) {
-  if (typeof options.spawn !== "function") {
-    throw "options.spawn was a(n) " + (typeof options.spawn) + " instead of a function.";
+  console.log('runCompiler');
+  if (typeof options.spawn !== 'function') {
+    throw 'options.spawn was a(n) ' +
+      typeof options.spawn +
+      ' instead of a function.';
   }
 
   var processArgs = prepareProcessArgs(sources, options);
   var processOpts = prepareProcessOpts(options);
 
-  console.log(["Running", pathToMake].concat(processArgs || []).join(" "));
-  
+  console.log(['Running', pathToMake].concat(processArgs || []).join(' '));
 
   return options.spawn(pathToMake, processArgs, processOpts);
 }
 
 function handleCompilerError(err, pathToMake) {
-  if ((typeof err === "object") && (typeof err.code === "string")) {
+  if (typeof err === 'object' && typeof err.code === 'string') {
     handleError(pathToMake, err);
   } else {
-    console.error("Exception thrown when attempting to run Elm compiler " + JSON.stringify(pathToMake) + ":\n" + err);
+    console.error(
+      'Exception thrown when attempting to run Elm compiler ' +
+        JSON.stringify(pathToMake) +
+        ':\n' +
+        err,
+    );
   }
   throw err;
 
@@ -90,15 +99,19 @@ function compile(sources, options) {
   var optionsWithDefaults = prepareOptions(options, spawn);
   var pathToMake = options.pathToMake || compilerBinaryName;
 
+  console.log(optionsWithDefaults);
+  console.log(pathToMake);
 
   try {
-    return runCompiler(sources, optionsWithDefaults, pathToMake)
-      .on('error', function(err) {
+    return runCompiler(sources, optionsWithDefaults, pathToMake).on(
+      'error',
+      function(err) {
         console.log(err);
         handleError(pathToMake, err);
 
         process.exit(1);
-      });
+      },
+    );
   } catch (err) {
     console.log(err);
     handleCompilerError(err, pathToMake);
@@ -110,25 +123,25 @@ function compile(sources, options) {
 // If you want html instead of js, use options object to set
 // output to a html file instead
 // creates a temp file and deletes it after reading
-function compileToString(sources, options){
-  if (typeof options.output === "undefined"){
+function compileToString(sources, options) {
+  if (typeof options.output === 'undefined') {
     options.output = '.js';
   }
 
-  return new Promise(function(resolve, reject){
-    temp.open({ suffix: options.output }, function(err, info){
-      if (err){
+  return new Promise(function(resolve, reject) {
+    temp.open({ suffix: options.output }, function(err, info) {
+      if (err) {
         return reject(err);
       }
-      console.log(fs.existsSync('/home/nowuser/src/src/elm/Main.elm'))
+      console.log(fs.existsSync('/home/nowuser/src/src/elm/Main.elm'));
       options.output = './dist/123' + options.output;
-      options.processOpts = { stdio: 'pipe' }
-      console.log(sources)
-      console.log(options)
+      options.processOpts = { stdio: 'pipe' };
+      console.log(sources);
+      console.log(options);
       var compiler = compile(sources, options);
 
-      compiler.stdout.setEncoding("utf8");
-      compiler.stderr.setEncoding("utf8");
+      compiler.stdout.setEncoding('utf8');
+      compiler.stderr.setEncoding('utf8');
 
       var output = '';
       compiler.stdout.on('data', function(chunk) {
@@ -139,61 +152,77 @@ function compileToString(sources, options){
       });
       console.log('AAAA');
       console.log(output);
-      compiler.on("close", function(exitCode) {
+      compiler.on('close', function(exitCode) {
+        console.log(output);
+        if (exitCode !== 0) {
+          return reject(new Error('Compilation failed\n' + output));
+        } else if (options.verbose) {
           console.log(output);
-          if (exitCode !== 0) {
-            return reject(new Error('Compilation failed\n' + output));
-          } else if (options.verbose) {
-            console.log(output);
-          }
+        }
 
-          fs.readFile(info.path, {encoding: "utf8"}, function(err, data){
-            return err ? reject(err) : resolve(data);
-          });
+        fs.readFile(info.path, { encoding: 'utf8' }, function(err, data) {
+          return err ? reject(err) : resolve(data);
         });
+      });
     });
   });
 }
 
 function handleError(pathToMake, err) {
-  if (err.code === "ENOENT") {
-    console.error("Could not find Elm compiler \"" + pathToMake + "\". Is it installed?")
-  } else if (err.code === "EACCES") {
-    console.error("Elm compiler \"" + pathToMake + "\" did not have permission to run. Do you need to give it executable permissions?");
+  if (err.code === 'ENOENT') {
+    console.error(
+      'Could not find Elm compiler "' + pathToMake + '". Is it installed?',
+    );
+  } else if (err.code === 'EACCES') {
+    console.error(
+      'Elm compiler "' +
+        pathToMake +
+        '" did not have permission to run. Do you need to give it executable permissions?',
+    );
   } else {
-    console.error("Error attempting to run Elm compiler \"" + pathToMake + "\":\n" + err);
+    console.error(
+      'Error attempting to run Elm compiler "' + pathToMake + '":\n' + err,
+    );
   }
 }
 
 // Converts an object of key/value pairs to an array of arguments suitable
 // to be passed to child_process.spawn for elm-make.
 function compilerArgsFromOptions(options, emitWarning) {
-  return _.flatten(_.map(options, function(value, opt) {
-    if (value) {
-      switch(opt) {
-        case "yes":    return ["--yes"];
-        case "help":   return ["--help"];
-        case "output": return ["--output", value];
-        case "report": return ["--report", value];
-        case "warn":   return ["--warn"];
-        case "debug":  return ["--debug"];
-        default:
-          if (supportedOptions.indexOf(opt) === -1) {
-            emitWarning('Unknown Elm compiler option: ' + opt);
-          }
+  return _.flatten(
+    _.map(options, function(value, opt) {
+      if (value) {
+        switch (opt) {
+          case 'yes':
+            return ['--yes'];
+          case 'help':
+            return ['--help'];
+          case 'output':
+            return ['--output', value];
+          case 'report':
+            return ['--report', value];
+          case 'warn':
+            return ['--warn'];
+          case 'debug':
+            return ['--debug'];
+          default:
+            if (supportedOptions.indexOf(opt) === -1) {
+              emitWarning('Unknown Elm compiler option: ' + opt);
+            }
 
-          return [];
+            return [];
+        }
+      } else {
+        return [];
       }
-    } else {
-      return [];
-    }
-  }));
+    }),
+  );
 }
 
 module.exports = {
   compile: compile,
   compileSync: compileSync,
-  compileWorker: require("./worker.js")(compile),
+  compileWorker: require('./worker.js')(compile),
   compileToString: compileToString,
-  findAllDependencies: findAllDependencies
+  findAllDependencies: findAllDependencies,
 };
